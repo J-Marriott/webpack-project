f90b2c20d19b25598fc17fd31d23d7ec0de8fbb0
Joe Marriott <joe@j-marriott.com>
2017-09-18 12:03:08 +0100

    vendor bundles wioth html-webpack-plugin dist/index.html to launch

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const VENDOR_LIBS = [
 	'react', 'lodash', 'redux', 'react-redux', 'react-dom', 'react-input-range', 'redux-form', 'redux-thunk'
@@ -12,7 +13,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: '[name].[chunkhash].js'
   },
   module: {
   	rules: [
@@ -30,7 +31,10 @@ module.exports = {
   },
   plugins: [
   	new webpack.optimize.CommonsChunkPlugin({
-  		name: 'vendor', filename: 'vendor.bundle.js'
+  		names: ['vendor', 'manifest']
+  	}),
+  	new HtmlWebpackPlugin({
+  		template: 'src/index.html'
   	})
   ]
 };
